c9aa6faf3158e97b18900166b4b2c151642982d8
Lone-Hassan <lone.hassan@gmail.com>
2024-05-03 00:16:28 +0500

    auto CICD check

--- src/test/java/loneCreates/InvalidLoginTest.java
+++ src/test/java/loneCreates/InvalidLoginTest.java
@@ -15,7 +15,7 @@ public class InvalidLoginTest extends BaseTest2 {
 	@Test(groups ="itemcheck")
 	public void InvalidLogin() throws IOException {
 		// TODO Auto-generated method stub
-
+		System.out.println("code added for CICD figures crossed");
 		//DashBoard db = launchDashBoard("https://cloud.tarsil.pk/", "03216466500", "lone@tersil");
 		//Assert.assertEquals(db.toastMsg(), "Username or Password is not correct.");
 		Assert.assertEquals("Username or Password is not correct.", "Username or Password is not correct.");
